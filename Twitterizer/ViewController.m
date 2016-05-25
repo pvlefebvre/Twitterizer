75db5269c630989d77b6d1fd98611e784f96f667
Amir Bahadori <pbahadori156@gmail.com>
2016-05-25 15:56:18 -0500

    Part 3,4 Done

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *characterCount;
 
 @end
 
@@ -19,6 +20,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+// Step 2: We loop over the characters in our text and remove the vowels a,e,i,o,u
+// THen we set the text of the UITextView to the newly created string without vowels
 - (IBAction)twitterize:(id)sender {
     NSString *text = self.textView.text;
     NSMutableString *noVowelsText = [[NSMutableString alloc] init];
@@ -33,5 +37,25 @@
     }
     self.textView.text = noVowelsText;
 }
+
+// Step 3: Whenever the user types a character, update the character count in the UILabel
+-(void)textViewDidChange:(UITextView *)textView {
+    self.characterCount.text = [NSString stringWithFormat:@"%lu", textView.text.length];
+    
+    
+    // Step 4: Limit us from typing past 140 characters
+    self.textView.text = (self.textView.text.length > 139 ? [self.textView.text substringToIndex:139] : self.textView.text);
+    
+    // Stretch 1: Beginning with the first word in the UITextView, make every other word begin with a “#”
+    
+}
+
+
+
+
+
+
+
+
 
 @end
