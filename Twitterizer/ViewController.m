f7c4021760e90d12c2b8a9ebe0d5acb87e5657ee
Paul Lefebvre <paullefebvre@Pauls-MacBook-Pro.local>
2016-05-25 15:22:24 -0500

    Part 2 complete

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -19,5 +19,19 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)twitterize:(id)sender {
+    NSString *text = self.textView.text;
+    NSMutableString *noVowelsText = [[NSMutableString alloc] init];
+    
+    for (int i = 0; i < text.length; i++) {
+        char c = [text characterAtIndex:i];
+        
+        if (c != 'a' && c != 'e' && c != 'i' && c !='o' && c != 'u' && c != 'A' && c != 'E' && c != 'I' && c !='O' && c != 'U') {
+            [noVowelsText appendFormat:@"%c",c];
+            
+        }
+    }
+    self.textView.text = noVowelsText;
+}
 
 @end
